b5a062bf2a970339bf31cd734a4ec50243dc7d17
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-19 09:18:52 +0530

    Fix hoisting example in 01_hoisting.js

--- 01_hoisting.js
+++ 01_hoisting.js
@@ -8,3 +8,16 @@ function printM() {
     console.log("Hello World")
 }
 
+
+// Hoisting for let and const is different
+
+console.log(a)  // Here it will give an error which says that cannot access a before initialization
+// That is because a is in the temporal dead zone, and a is not attached to the global window object.
+let a = 10
+
+
+// Same goes to const
+// Both let and const are hoisted
+
+
+
